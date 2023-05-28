ae4facfc168577f67969d528c204682c79e31c7c
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-05-28 15:10:42 +0900

    2.2 Redirect and Rewrite

--- src/components/HeadTitle.js
+++ src/components/HeadTitle.js
@@ -1,10 +1,6 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function HeadTitle({ title }) {
-  console.log("------------------------------------");
-  console.log("title:", title);
-  console.log(typeof title);
-  console.log("------------------------------------");
   return (
     <Head>
       <title>{title} | Next Movies</title>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,58 @@
-import { useEffect, useState } from "react";
-import HeadTitle from "@/components/HeadTitle";
-
-// Movie DB Free Api Key
-const API_KEY = "0893f8c7df602588a4cbc8304965ecab";
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
-
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
 
   return (
     <>
-      <div>
+      <div className="container">
         {!movies && <h4>Loading...</h4>}
         {movies?.map((movie) => (
-          <div key={movie.id}>
+          <div className="movie" key={movie.id}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.original_title}
+              width={300}
+              height={350}
+              style={{
+                'max-width': '100%',
+                'border-radius': '12px',
+                transition: 'transform 0.2s ease-in-out',
+                'box-shadow': 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
+              }}
+            />
             <h4>{movie.original_title}</h4>
           </div>
         ))}
       </div>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </>
   );
 }
